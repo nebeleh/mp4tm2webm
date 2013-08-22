4112c4312c2d87e34176a9c94f6798c7217099eb
Saman Amirpour Amraii <nebeleh@gmail.com>
2013-08-22 14:18:41 -0400

    ignoring mp4 files with local in their name. those files are corrupted

--- converter.cpp
+++ converter.cpp
@@ -30,58 +30,62 @@ bool file_exists(const char *path)
 // loading a directory, convertin mp4s and copying everything to new directory
 int loadData(const char *sourcePath, const char *destPath)
 {
-	struct dirent *entry;
-	DIR *dp;
+  struct dirent *entry;
+  DIR *dp;
 
-	dp = opendir(sourcePath);
-	if (dp == NULL)
-	{
-		perror("opendir");
-		return -1;
-	}
+  dp = opendir(sourcePath);
+  if (dp == NULL)
+  {
+    perror("opendir");
+    return -1;
+  }
 
-	string f,t,fullPath;
-	struct stat st;
-	while((entry = readdir(dp)))
-	{
-		f = (string)entry->d_name;
+  string f,t,fullPath;
+  struct stat st;
+  while((entry = readdir(dp)))
+  {
+    f = (string)entry->d_name;
     if (f == "." || f == "..") continue;
-		fullPath = (string)sourcePath+'/'+f;
-		stat(fullPath.c_str(), &st);
-		
-		// if this is a directory or a symlink, go inside it and make a similar structure in destination
-		if((S_ISDIR(st.st_mode) || S_ISLNK(st.st_mode)))
-		{
+    fullPath = (string)sourcePath+'/'+f;
+    stat(fullPath.c_str(), &st);
+
+    // if this is a directory or a symlink, go inside it and make a similar structure in destination
+    if((S_ISDIR(st.st_mode) || S_ISLNK(st.st_mode)))
+    {
       mkdir(((string)destPath+'/'+f).c_str(),0777);
-			loadData(fullPath.c_str(), ((string)destPath+'/'+f).c_str());
-		}
+      loadData(fullPath.c_str(), ((string)destPath+'/'+f).c_str());
+    }
     // if this is an mp4 file, append black video, convert it to webm and copy it
-		else if(S_ISREG(st.st_mode) && f.length() > 4 && f.substr(f.length()-3)=="mp4")
-		{
-			// adding black frames
+    else if(S_ISREG(st.st_mode) && f.length() > 4 && f.substr(f.length()-3)=="mp4")
+    {
+      // there are some video files with 'local' in their name. they are corrupted and I don't have to convert them
+      size_t found = f.find("local");
+      if (found != string::npos) continue;
+
+      // adding black frames
       cout << fullPath << ": adding black frames, " << flush;
-      sprintf(buf,"./ffmpeg -y -i %s/black.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts %s/temp2 > /dev/null 2>&1 & ./ffmpeg -y -i %s -c copy -bsf:v h264_mp4toannexb -f mpegts %s/temp1 > /dev/null 2>&1 & ./ffmpeg -y -f mpegts -i \"concat:%s/temp1|%s/temp2\" -c copy %s/%s > /dev/null 2>&1",destTop,destTop,fullPath.c_str(),destTop,destTop,destTop,destPath,f.c_str());
+      sprintf(buf,"./ffmpeg -y -i %s/black.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts %s/temp2 &> /dev/null & ./ffmpeg -y -i %s -c copy -bsf:v h264_mp4toannexb -f mpegts %s/temp1 &> /dev/null & ./ffmpeg -y -f mpegts -i \"concat:%s/temp1|%s/temp2\" -c copy %s/%s &> /dev/null",destTop,destTop,fullPath.c_str(),destTop,destTop,destTop,destPath,f.c_str());
       system(buf);
-      
+
       // converting to webm
       cout << "encoding to yuv, " << flush;
-      sprintf(buf,"./ffmpeg -i %s -pix_fmt yuv420p %s/%s.yuv > /dev/null 2>&1", fullPath.c_str(), destPath, f.c_str());
+      sprintf(buf,"./ffmpeg -i %s -pix_fmt yuv420p %s/%s.yuv &> /dev/null", fullPath.c_str(), destPath, f.c_str());
       system(buf);
-      
+
       cout << "decoding to webm, pass 1, " << flush;
-      sprintf(buf,"./vpxenc %s/%s.yuv -o %s/%s.webm -w %d -h %d --fps=%ld/1 -p 2 --codec=vp8 --fpf=%s/%s.fpf --cpu-used=0 --target-bitrate=%ld --auto-alt-ref=1 -v --min-q=4 --max-q=52 --drop-frame=0 --lag-in-frames=25 --kf-min-dist=0 --kf-max-dist=120 --static-thresh=0 --tune=psnr --pass=1 --minsection-pct=0 --maxsection-pct=800 -t 0 > /dev/null 2>&1", destPath, f.c_str(), destPath, f.substr(0,f.length()-4).c_str(), width, height, lround(fps), destPath, f.substr(0,f.length()-4).c_str(), lround(width * height * fps * targetBitsPerPixel / 8000.0));
+      sprintf(buf,"./vpxenc %s/%s.yuv -o %s/%s.webm -w %d -h %d --fps=%ld/1 -p 2 --codec=vp8 --fpf=%s/%s.fpf --cpu-used=0 --target-bitrate=%ld --auto-alt-ref=1 -v --min-q=4 --max-q=52 --drop-frame=0 --lag-in-frames=25 --kf-min-dist=0 --kf-max-dist=120 --static-thresh=0 --tune=psnr --pass=1 --minsection-pct=0 --maxsection-pct=800 -t 0 &> /dev/null", destPath, f.c_str(), destPath, f.substr(0,f.length()-4).c_str(), width, height, lround(fps), destPath, f.substr(0,f.length()-4).c_str(), lround(width * height * fps * targetBitsPerPixel / 8000.0));
       system(buf);
-      
+
       cout << "pass 2, " << flush;
-      sprintf(buf,"./vpxenc %s/%s.yuv -o %s/%s.webm -w %d -h %d --fps=%ld/1 -p 2 --codec=vp8 --fpf=%s/%s.fpf --cpu-used=0 --target-bitrate=%ld --auto-alt-ref=1 -v --min-q=4 --max-q=52 --drop-frame=0 --lag-in-frames=25 --kf-min-dist=0 --kf-max-dist=120 --static-thresh=0 --tune=psnr --pass=2 --minsection-pct=5 --maxsection-pct=1000 --bias-pct=50 -t 6 --end-usage=vbr --good --profile=0 --arnr-maxframes=7 --arnr-strength=5 --arnr-type=3 --psnr > /dev/null 2>&1", destPath, f.c_str(), destPath, f.substr(0,f.length()-4).c_str(), width, height, lround(fps), destPath, f.substr(0,f.length()-4).c_str(), lround(width * height * fps * targetBitsPerPixel / 8000.0));
+      sprintf(buf,"./vpxenc %s/%s.yuv -o %s/%s.webm -w %d -h %d --fps=%ld/1 -p 2 --codec=vp8 --fpf=%s/%s.fpf --cpu-used=0 --target-bitrate=%ld --auto-alt-ref=1 -v --min-q=4 --max-q=52 --drop-frame=0 --lag-in-frames=25 --kf-min-dist=0 --kf-max-dist=120 --static-thresh=0 --tune=psnr --pass=2 --minsection-pct=5 --maxsection-pct=1000 --bias-pct=50 -t 6 --end-usage=vbr --good --profile=0 --arnr-maxframes=7 --arnr-strength=5 --arnr-type=3 --psnr &> /dev/null", destPath, f.c_str(), destPath, f.substr(0,f.length()-4).c_str(), width, height, lround(fps), destPath, f.substr(0,f.length()-4).c_str(), lround(width * height * fps * targetBitsPerPixel / 8000.0));
       system(buf);
-      
+
       // deleting mp4, fpf and yuv files
       cout << "deleting temp files, " << flush;
       sprintf(buf,"rm %s/%s.yuv %s/%s.fpf -f", destPath, f.c_str(), destPath, f.substr(0,f.length()-4).c_str());
       system(buf);
       cout << "done!" << endl;
-		}
+    }
     // otherwise copy it
     else
     {
@@ -89,10 +93,10 @@ int loadData(const char *sourcePath, const char *destPath)
       sprintf(buf,"cp %s %s",fullPath.c_str(),destPath);
       system(buf);
     }
-	}
+  }
 
-	closedir(dp);
-	return 0;
+  closedir(dp);
+  return 0;
 }
 
 // do some preparation
@@ -104,14 +108,14 @@ int prepareData(const char *sourcePath, const char *destPath)
     cout << "Didn't find ffmpeg and/or vpxenc in local folder" << endl;
     return -1;
   }
-  
+
   // check whether source directory exists
   if (!file_exists(sourcePath))
   {
     cout << "Source folder doesn't exists." << endl;
     return -1;
   }
-  
+
   // check whether source directory has r.json
   sprintf(buf,"%s/r.json",sourcePath);
   if (!file_exists(buf))
@@ -119,7 +123,7 @@ int prepareData(const char *sourcePath, const char *destPath)
     cout << "r.json doesn't exist in source folder." << endl;
     return -1;
   }
-  
+
   // read r.json and find width and height of video
   FILE *in = fopen(buf, "rb");
   if (in)
@@ -132,14 +136,14 @@ int prepareData(const char *sourcePath, const char *destPath)
     contents=(char *)malloc(len); //malloc buffer
     fread(contents,len,1,in); //read into buffer
     fclose(in);
-    
+
     picojson::value v;
     string err;
     picojson::parse(v, contents, contents + strlen(contents), &err);
     if (! err.empty()) {
       cout << err << endl;
     }
-    
+
     // check if the type of the value is "object"
     if (! v.is<picojson::object>()) {
       cout << "JSON is not an object" << endl;
@@ -156,13 +160,13 @@ int prepareData(const char *sourcePath, const char *destPath)
       if (i->first == "video_width") { width = atoi(i->second.to_str().c_str()); }
       if (i->first == "fps") { fps = atof(i->second.to_str().c_str()); }
     }
-    
+
     if (height == -1 || width == -1 || fps == -1)
     {
       cout << "not enough inputs in JSON." << endl;
       return -1;
     }
-    
+
     cout << "width: " << width << ", height: " << height << ", fps: " << fps << endl;
   }
   else
@@ -170,14 +174,14 @@ int prepareData(const char *sourcePath, const char *destPath)
     cout << "Cannot open r.json" << endl;
     return -1;
   }
-  
+
   // check for destination folder; it shouldn't exist
   if (file_exists(destPath))
   {
     cout << "Destination folder already exists. Please first remove it." << endl;
     return -1;
   }
-  
+
   // create destination folder
   cout << "Creating destination folder ..." << endl;
   umask(~0777);
@@ -186,26 +190,26 @@ int prepareData(const char *sourcePath, const char *destPath)
     perror("mkdir");
     return -1;
   }
-  
+
   // create a 10 frame, 1 second black video in correct width and height
   sprintf(buf,"./ffmpeg -t 1 -s %dx%d -vcodec rawvideo -f rawvideo -pix_fmt rgb24 -r 10 -i /dev/zero -vcodec libx264 -preset slow -pix_fmt yuv420p %s/black.mp4 > /dev/null 2>&1",width,height,destPath);
   cout << "Creating black frames ..." << endl;
   system(buf);
-  
+
   // create fifo files
   sprintf(destTop,"%s",destPath);
   cout << "Creating temporary fifo files ..." << endl;
   sprintf(buf,"mkfifo %s/temp1 %s/temp2", destTop, destTop);
   system(buf);
-  
+
   // recursively check for files, add black frames, convert mp4s
   int res = loadData(sourcePath, destPath);
-  
+
   // delete fifo files and black video
   cout << "Deleting temporary files ..." << endl;
   sprintf(buf,"rm -f %s/temp1 %s/temp2 %s/black.mp4",destTop, destTop, destTop);
   system(buf);
-  
+
   // finish
   return res;
 }
@@ -215,25 +219,18 @@ int main(int argc, char **argv)
   // check whether there are right amount of parameters
   if (argc != 4)
   {
-    cout << "incorrect parameters, usage is: " << endl;
+    cout << "incorrect parameters, usage: " << endl;
     cout << "./converter sourcevideos_path destvideos_path targetBitsPerPixel" << endl;
     return -1;
   }
-  
+
   // do computations using source and destination folder
   targetBitsPerPixel = atof(argv[3]);
   int res = prepareData(argv[1], argv[2]);
-  
+
   if (!res)
     cout << "Converting finished successfully." << endl;
-  
+
   return res;
 }
-
-
-
-
-
-
-
 
